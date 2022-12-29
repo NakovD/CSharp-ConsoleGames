b914b1e7ece3b09b5ab31e609e24609af5b7f1ce
David Nakov <nakov_david@mail.bg>
2022-12-29 12:15:45 +0200

    feat: moved most of the global game logic in a game engine class;

---
+++ BattleshipGame/BattleshipGame/Core/GameEngine.cs
@@ -0,0 +1,49 @@
+﻿namespace BattleshipGame.Core
+{
+    using Enums;
+    using Models;
+    using Models.Grid;
+    using Models.Ship;
+
+    using ConsoleInitialSetup;
+
+    using System.Text;
+
+    public class GameEngine
+    {
+        public void Run()
+        {
+            ConsoleSetup.Configure();
+            Console.InputEncoding = Encoding.Unicode;
+            Console.CursorVisible = false;
+
+            var gridCols = 21;
+            var gridRows = 21;
+
+            var spaceBetweenGrids = 30;
+            var centerX = (Console.WindowWidth - (gridCols * 2) - spaceBetweenGrids / 2) / 2;
+            var centerY = ((Console.WindowHeight - gridRows) / 2);
+
+            var playerGrid = new Grid(centerX, centerY);
+            var emptyCells = playerGrid.Draw().OrderBy(c => c.x);
+
+            var opponentGrid = new Grid(centerX + spaceBetweenGrids, centerY);
+            var opponentCells = opponentGrid.Draw();
+
+            var ship = new Ship("Submarine", playerGrid.Boundaries);
+            ship.SetCoordinates(emptyCells.Take(3).Select(c => new Cell(c.x, c.y)).ToList());
+            ship.Draw();
+
+            while (true)
+            {
+                var key = Console.ReadKey().Key;
+
+                if (key == ConsoleKey.UpArrow) ship.Move(Direction.Up);
+                if (key == ConsoleKey.DownArrow) ship.Move(Direction.Down);
+                if (key == ConsoleKey.LeftArrow) ship.Move(Direction.Left);
+                if (key == ConsoleKey.RightArrow) ship.Move(Direction.Right);
+                continue;
+            }
+        }
+    }
+}
--- BattleshipGame/BattleshipGame/Program.cs
+++ BattleshipGame/BattleshipGame/Program.cs
@@ -1,56 +1,13 @@
 ﻿namespace Battleship
 {
-    using ConsoleInitialSetup;
+    using BattleshipGame.Core;
 
-    using System;
-    using System.Text;
-
-    internal class Program
+    public class Program
     {
-        private static int gridRows = 21;
-        private static int gridCols = 31;
-
         private static void Main(string[] args)
         {
-            ConsoleSetup.Configure();
-            Console.InputEncoding = Encoding.Unicode;
-
-            CreateGrid();
-        }
-
-        private static void CreateGrid()
-        {
-            var rows = gridRows;
-            var cols = gridCols;
-
-            for (int row = 0; row < rows; row++)
-            {
-                for (int col = 0; col < cols; col++)
-                {
-                    string symbol = GetSymbol(row, col);
-                    Console.Write(symbol);
-                }
-                Console.WriteLine();
-            }
-        }
-
-        private static string GetSymbol(int row, int col)
-        {
-            var rows = gridRows - 1;
-            var cols = gridCols - 1;
-
-            if (row == 0 && col == 0) return "\u250C";  //top left corner
-            if (row == 0 && col == cols) return "\u2510"; //top right corner
-            if (row == rows && col == 0) return "\u2514"; //bottom right corner
-            if (row == rows && col == cols) return "\u2518";    //bottom left corner
-            if (col == 0 && row % 2 == 0) return "\u251C";      //all left middle cells
-            if (col == cols && row % 2 == 0) return "\u2524";   //all right middle cells
-            if (row == 0 && col % 3 == 0) return "\u252C";      //all top middle cells
-            if (row == rows && col % 3 == 0) return "\u2534";   //all bottom middle cells
-            if (col % 3 == 0 && row % 2 != 0) return "\u2502";
-            if (row % 2 == 0 && col % 3 != 0) return "\u2500";
-            if (col % 3 == 0 && row % 2 == 0) return "\u253C";  //all cells cross cells
-            return " ";
+            var gameEngine = new GameEngine();
+            gameEngine.Run();
         }
     }
 }
